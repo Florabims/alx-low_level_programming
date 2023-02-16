e52c586b0fb0457b25e398856bbd64ca7a50d70a
Florence <florence.osunnuga@gmail.com>
2023-02-16 15:35:41 -0800

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-main - Entry  
-return: Always 0
-imt main(void)
+/**
+*main-Entry  
+*Return: Always 0
+*/
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\ln");
-	retun (0);
-}
-
-			
+printf("with proper grammar, but the outcome is a piece of art,\ln");
+return (0);
+}	
